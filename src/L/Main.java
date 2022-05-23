e0cd21cbc8f5af3997e995e2289761e3dcd32d66
Robin Stenskytt <robin.olivemark@gmail.com>
2022-05-23 12:51:17 +0200

    Init

---
+++ src/L/Main.java
@@ -0,0 +1,9 @@
+package L;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Why are you snooping about in this repo lmao
+	System.out.println("Why are you here?");
+    }
+}
